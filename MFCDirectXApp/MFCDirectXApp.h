d84b15d6857e338aae31cb7c8410d54f68fa5835
dryerem <73961037+dryerem@users.noreply.github.com>
2022-05-21 18:47:55 +0300

    DirectX 9 MFC Binding
    Просто подключил DirectX 9 к MFC приложению

---
+++ MFCDirectXApp/MFCDirectXApp.h
@@ -0,0 +1,32 @@
+﻿
+// MFCDirectXApp.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CMFCDirectXAppApp:
+// Сведения о реализации этого класса: MFCDirectXApp.cpp
+//
+
+class CMFCDirectXAppApp : public CWinApp
+{
+public:
+	CMFCDirectXAppApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCDirectXAppApp theApp;
